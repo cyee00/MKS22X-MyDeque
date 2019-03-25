ec726b876a482407834c0c7cd3597283f9c15e78
cyee00 <cyee00@stuy.edu>
2019-03-25 17:40:13 -0400

    calc

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,25 @@
 public class Calculator{
+    public Calculator(){}
+
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
-    public static double eval(String s){
-      new String[] data = s.split(" ");
+    //public static String eval(String s){
+    public String eval(String s){
+      String[] data = s.split(" ");
+      return toString(data);
+    }
+
+    private String toString(String[]data1){
+      String ans="";
+      for (int i=0;i<data1.length;i++){
+        ans+=data1[i];
+      }
+      return ans;
+    }
+
+    public static void main(String[] args){
+      Calculator calc = new Calculator();
+      System.out.println(calc.eval(args[0]));
     }
 }
